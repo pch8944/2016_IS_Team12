308194279d452ecb91bee81843897f24248a6840
irishev <irishev@naver.com>
2016-11-25 20:53:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from sqlinjection import SQL
+from authentication_session_management import auth_sess
 
 #SQL Injection - target url, post parameter need
 sqlinj = SQL()
 sqlinj.proc(url,post)
+
+#auth_sess - file name, url need
+asm = auth_sess()
+asm.inpection(file_name,url)
