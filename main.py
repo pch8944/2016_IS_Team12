8b353df26c7f5b054425588c32c7f93665fb3bea
pch8944 <pch8944@gmail.com>
2016-12-08 17:11:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from sqlinjection import SQL
 from authentication_session_management import auth_sess
 from secure_file_uploader import FileUploader
 
+def f_search(self, f_name):
+    filenames = os.listdir(f_name)
+    if not os.path.isdir(f_name):
+        print("Error: cannot find directory")
+        return 0
+    else:
+        return filenames
+
 #SQL Injection - target url, post parameter need
 sqlinj = SQL()
 sqlinj.proc(url,post)
