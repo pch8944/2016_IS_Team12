debe202f38f2d30f64f10b2502a7fdc530dbd9d3
pch8944 <pch8944@gmail.com>
2016-12-08 17:32:50 +0900

    Update authentication_session_management.py

--- authentication_session_management.py
+++ authentication_session_management.py
@@ -5,28 +5,15 @@ import webbrowser
 from bs4 import BeautifulSoup
 
 class auth_sess:
-    #folder search
-    def f_search(self, f_name):
-        filenames = os.listdir(f_name)
-        if not os.path.isdir(f_name):
-            print("Error: cannot find directory")
-            return 0
-        else:
-            return filenames
-
     # file open
     def inspection(self, folder_name, file_name, edit_url):
-        # target = open("test.php", 'r')
-
-        # get url
-        # edit_url = input("url for modification : ")
-        # edit_url = "http://www.naver.com"
+        f_result = open('result.txt', 'w')
 
         if edit_url.find('http') == -1:
             edit_url = "http://" + edit_url
 
         # cookie inspection
-        print("#1. Cookie inspection")
+        f_result.write("#1. Cookie inspection\n")
 
         for filename in file_name:
             target = open(folder_name + "/" + filename, "r")
@@ -41,7 +28,6 @@ class auth_sess:
             cookie_point = 0
             while cookie_point >= 0:
                 cookie_point = str1.find('cookie', cookie_point)
-                # print(cookie_point)
                 if cookie_point == -1:
                     break
                 key1 = str1.find('\"', cookie_point)
@@ -51,15 +37,15 @@ class auth_sess:
                 if key2 == -1:
                    key2 = str1.find('\'', key1+1)
 
-                print "cookie " + str1[key1:key2+1] + " is can be attacked by cookie injection"
-                print "vulnerable code: " + result[index].lstrip()
+                f_result.write("cookie " + str1[key1:key2+1] + " is can be attacked by cookie injection\n")
+                f_result.write("vulnerable code: " + result[index].lstrip() + "\n")
                 cookie_point += 1
                 index += 1
             target.close()
 
 
         # url jumping inspection (have to know board URL)
-        print("\n#2. URL jumping inspection")
+        f_result.write("\n#2. URL jumping inspection\n")
 
         # soup = BeautifulSoup(urllib.request.urlopen('http://kupa.korea.ac.kr/btbkplus/commu/notice3.do?mode=edit&articleNo=22559&article.offset=0&articleLimit=10').read(), "lxml")
         # editData = soup.find_all('div', {'class': "login"})
@@ -70,17 +56,14 @@ class auth_sess:
         text_area = soup.find_all('textarea')
         edit_area = soup.find_all('iframe')
 
-        # print(text_area)
-        # print(edit_area)
-
-        if len(text_area)!=0 or len(edit_area)!=0:
-            print("This web site have url jumping vulnerability")
+        if len(text_area)==0 and len(edit_area)==0:
+            f_result.write("This web site have url jumping vulnerability\n")
         else:
-            print("This web site have no url jumping vulnerability")
+            f_result.write("This web site have no url jumping vulnerability\n")
 
 
         # time out inspection (have to detect function file)
-        print("\n#3. Timeout function inspection")
+        f_result.write("\n#3. Timeout function inspection\n")
 
         no_timeout = 0
         for filename in file_name:
@@ -95,13 +78,8 @@ class auth_sess:
             target.close()
 
         if no_timeout != 1:
-            print("Timeout function is not implemented in this web site")
+            f_result.write("Timeout function is not implemented in this web site\n")
         else:
-            print("Timeout function is implemented in this web site")
+            f_result.write("Timeout function is implemented in this web site\n")
 
-# f_name = raw_input()
-# asm = auth_sess()
-# filenames = asm.f_search(f_name)
-
-# if len(filenames) != 0:
-    # asm.inspection(f_name, filenames, "www.naver.com")
+        f_result.close()
