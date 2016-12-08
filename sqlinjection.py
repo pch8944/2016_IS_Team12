88b05c34f114b4e4049cecfbac6956ddf8c5a37b
irishev <irishev@naver.com>
2016-12-08 17:54:45 +0900

    Add files via upload

--- sqlinjection.py
+++ sqlinjection.py
@@ -4,23 +4,65 @@ import sys
 import time
 
 class SQL:
-    def proc(url,post):
+    def fileopen(self,path):
+        global postl
+        postl = []
+        f = open(path,'r')
+        get = []
+        while 1:
+            line = f.readline()
+            if not line: break
+            p = line.find('$_GET')
+            e=p
+            param=''
+            if p!=-1 :
+                while 1:
+                    if(line[p]=='"'): break
+                    if(line[p]=="'"): break
+                    p=p+1
+                e=p+1
+                while 1:
+                    if(line[e]=='"'): break
+                    if(line[e]=="'"): break
+                    e=e+1
+                param = line[p+1:e]
+                get.append(param)
+            p = line.find('$_POST')
+            e=p
+            param=''
+            if p!=-1 :
+                while 1:
+                    if(line[p]=='"'): break
+                    if(line[p]=="'"): break
+                    p=p+1
+                e=p+1
+                while 1:
+                    if(line[e]=='"'): break
+                    if(line[e]=="'"): break
+                    e=e+1
+                param = line[p+1:e]
+                postl.append(param)
+
+        get = list(set(get))
+        postl = list(set(postl))
+        extra = '?'
+        for i in range(len(get)):
+            extra = extra + get[i] + '=&'
+        extra = extra[0:-1]
+        return extra
+    def proc(self,url,path):
+        f=open('sqlreport.txt','w')
         gettest=1
-        issp=0
-        url = input("target url : ")
-                getp = url.find('?')
-                while getp==-1:
-                    print 'There are no GET parameters. GET test will be skipped. Do you want to proceed anyway? [Y/N]'
-                    ch = input()
-                    if(ch=='Y'):
-                        gettest=0
-                        break
-                    else(ch=='N'):
-                        exit()
-        if post!=0
-            issp=1
-        
-        print '...'
+        issp=1
+        post = ''
+        getp = url.find('?')
+        if getp==-1:
+            f.write('There are no GET parameters. GET test will be skipped.\n')
+            gettest=0   
+        if not postl:
+            issp=0
+        else: post = postl[0]
+        print postl
         if gettest==1:
             get = ''
             if(getp==-1):
@@ -37,7 +79,7 @@ class SQL:
                 response = urllib2.urlopen(req)
                 data2 = response.read()
                 if data1!=data2 and i!=2:
-                    sys.stdout.write('Union SQL injection #' + str(i) + ' DETECT')
+                    f.write('Union SQL injection #' + str(i) + ' DETECT\n')
                     break
                 data1=data2
             
@@ -47,7 +89,7 @@ class SQL:
                 response = urllib2.urlopen(req)
                 data2 = response.read()
                 if data1!=data2 and i!=2:
-                    sys.stdout.write('\nBoolean based SQL injection DETECT('+str(i)+')')
+                    f.write('Boolean based SQL injection DETECT('+str(i)+')\n')
                     break
                 data1=data2
             
@@ -58,7 +100,7 @@ class SQL:
             response = urllib2.urlopen(req)
             data2 = response.read()
             if data1!=data2:
-                sys.stdout.write('\nError based SQL injection DETECT')
+                f.write('Error based SQL injection DETECT\n')
             
             req = urllib2.Request(dest+get+urllib.quote(' and 1=1'))
             response = urllib2.urlopen(req)
@@ -67,7 +109,7 @@ class SQL:
             response = urllib2.urlopen(req)
             data2 = response.read()
             if data1!=data2:
-                sys.stdout.write('\nBlind SQL injection DETECT')
+                f.write('Blind SQL injection DETECT\n')
             
 
             t1=time.time()
@@ -84,7 +126,7 @@ class SQL:
             interval2 = t2-t1
 
             if interval2-interval1>0.9:
-                sys.stdout.write('\nTime based SQL injection DETECT')
+                f.write('Time based SQL injection DETECT\n')
 
         if issp==1:
             data1=''
@@ -94,7 +136,7 @@ class SQL:
                 response = urllib2.urlopen(req)
                 data2 = response.read()
                 if len(data1)!=len(data2) and i!=2:
-                    sys.stdout.write('\nBoolean based SQL injection DETECT('+str(i)+')')
+                    f.write('Boolean based SQL injection DETECT('+str(i)+')\n')
                     break
                 data1 = data2
                 
@@ -106,7 +148,7 @@ class SQL:
             t2=time.time()
 
             if t2-t1>4.5:
-                sys.stdout.write('\nTime based SQL injection DETECT')
+                f.write('Time based SQL injection DETECT')
                     
 
         print '\nDone'
